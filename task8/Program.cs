9114202c4f97fc36bda1ad8e6460c1ccb55a89a2
«Artur» <art13471118@gmail.com>
2022-12-01 02:56:40 +0300

    программа выдает последовательность четных чисел от 1 до введенного числа N(рассматривается вариант только положительных чисел)

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число ");
+double N = double.Parse(Console.ReadLine()!);
+int i = 1;
+Console.WriteLine("Последовательность четных чисел до " + N );
+while (i < N)
+    {
+if (i % 2 == 0)
+{
+     Console.WriteLine (i);  
+}
+    i = i + 1;
+    }
